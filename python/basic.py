cf375ae22333aa207919554a34ed81230eb3c6ca
Akshatjangid <akshatjangid9@gmail.com>
2025-06-13 10:54:12 +0530

    Updated files

--- python/basic.py
+++ python/basic.py
@@ -1,7 +1,9 @@
-a=10
-b=True                    #boolean
-c="hello"
+# a=10
+# b=True                    #boolean
+# c="hello"
 
-print(type(a))
-print(type(b))
-print(type(c))
+# print(type(a))
+# print(type(b))
+# print(type(c))
+def addfun(a,b):
+    return a+b
